d3478c6aec33deff45e86a3025ee6fd9f051c817
Aditya <aditya102adi@gmail.com>
2025-07-15 20:13:44 +0530

    init commit

--- src/Interfaces/Shopping.java
+++ src/Interfaces/Shopping.java
@@ -3,8 +3,8 @@ interface Member {
     void callBack();
 }
 
-class Shop {
-    Member[] mem = new Member[100];
+class Store {
+    Member[] mem = new Member[10];
     int count = 0;
     void register(Member m) {
         mem[count++] = m;
@@ -13,7 +13,27 @@ class Shop {
         for(int i=0;i<count;i++)
             mem[i].callBack();
     }
+}
 
+class Customer implements Member {
+    String name;
+    Customer(String name) {
+        this.name = name;
+    }
+
+    public void callBack(){
+        System.out.println(name+" will visit the store");
+    }
 }
 public class Shopping {
+    public static void main(String[] args) {
+        Store s = new Store();
+        Customer c1 = new Customer("John");
+        Customer c2 = new Customer("Smith");
+
+        s.register(c1);
+        s.register(c2);
+
+        s.inviteSal();
+    }
 }
